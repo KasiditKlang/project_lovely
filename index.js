f8cfffefe04726a7689068c44324f87bf1a615f2
KasiditKlang <kasidit.ma@kkumail.com>
2024-09-19 16:28:25 +0700

    index

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/data.json', (req, res) => {
 });
 
 // Serve the HTML file
-app.get('/', (req, res) => {
+app.get('/rndFood.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'rndfood.html'));
 });
 
